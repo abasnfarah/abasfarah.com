9334a168a8f115c00290108f9f07933c260a6c6d
Abas Farah (server side) <abasnfarah@gmail.con>
2016-04-06 20:13:20 -0500

    newfile server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+/*
+var http = require('http'),
+fs = require('fs');
+
+
+fs.readFile('./LandingPage.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
+*/
+
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(index);
+}).listen(8000);
+
+
+
+
+
+
+
