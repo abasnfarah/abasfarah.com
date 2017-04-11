e7238df09df3ce3029bdab38cf496fde3e2fd653
abasnfarah <abasnfarah@gmail.com>
2017-04-11 19:04:03 +0000

    blog Models update

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,15 +3,15 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-#    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default="Abas Farah")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default="Abas Farah")
     title = models.CharField(max_length = 140)
     body = models.TextField()
-#    created_date = models.DateTimeField( default=timezone.now)
+    created_date = models.DateTimeField( default=timezone.now)
     date = models.DateTimeField()
 
-#    def publish(self):
-#        self.date=timezone.now()
-#        self.save()
+    def publish(self):
+        self.date=timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
