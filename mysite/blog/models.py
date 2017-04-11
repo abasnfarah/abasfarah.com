ff031ca9b4ca581e7e7aed05bf610d7d910f3772
abasnfarah <abasnfarah@gmail.com>
2017-04-11 18:54:05 +0000

    blog Models update

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -6,12 +6,12 @@ class Post(models.Model):
 #    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default="Abas Farah")
     title = models.CharField(max_length = 140)
     body = models.TextField()
-    created_date = models.DateTimeField( default=timezone.now)
+#    created_date = models.DateTimeField( default=timezone.now)
     date = models.DateTimeField()
 
-    def publish(self):
-        self.date=timezone.now()
-        self.save()
+#    def publish(self):
+#        self.date=timezone.now()
+#        self.save()
 
     def __str__(self):
         return self.title
